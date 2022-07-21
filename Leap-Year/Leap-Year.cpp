eb39b10b87abe841e77d683a6d6693f61dfb5eea
Noel Cruz <nocruz@activision.com>
2022-07-21 08:42:00 -0400

    wrote checkLeapYear function to return whether or not the user's input is a leap year.

--- Leap-Year/Leap-Year.cpp
+++ Leap-Year/Leap-Year.cpp
@@ -3,7 +3,27 @@
 #include <iostream>
 using namespace std;
 
+string checkLeapYear(int year) {
+	if (year % 400 == 0) {
+		return "a leap year.";
+	}
+	else if (year % 100 == 0) {
+		return "not a leap year.";
+	}
+	else if (year % 4 == 0) {
+		return "a leap year.";
+	}
+	else {
+		return "not a leap year.";
+	}
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+	int input;
+
+	cout << "- Leap Year Checker -" << endl;
+	cout << "Please enter a year to determine if it is a leap year:" << endl;
+	cin >> input;
+	cout << input << " is " << checkLeapYear(input) << endl;
 }
